f5c866a203afd3b176cc7cde1277a1ce7a83d06a
Madhura Kurhadkar <kurhadkar.m@northeastern.edu>
2023-10-02 18:51:25 -0400

    Added the healthz app

---
+++ route/route.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const healthController = require('../controller/healthController.js');
+
+
+// GET
+router.get('/healthz', healthController.getItems);
+//POST
+router.post('/healthz', healthController.createItem);
+//PUT
+router.put('/healthz', healthController.updateItem);
+//DELETE
+router.delete('/healthz', healthController.deleteItem);
+//PATCH
+router.patch('/healthz', healthController.patchItem);
+//any other request
+router.get('*', healthController.invalidRequestItem);
+
+
+module.exports = router
