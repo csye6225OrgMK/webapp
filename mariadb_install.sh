926a3686e40781a5a78e38aa8d6acddd0a1c9bb9
Madhura Sanjay Kurhadkar <kurhadkar.m@northeastern.edu>
2023-10-18 03:37:31 -0400

    update yml

--- mariadb_install.sh
+++ mariadb_install.sh
@@ -26,9 +26,12 @@ sudo mysql --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY 'root'; FLUSH 
 
 # mkdir /opt/madhura_kurhadkar_002769373_05
 
+sudo chmod -R 755 /opt
 sudo mkdir -p /opt/madhura_kurhadkar_002769373_05
 sudo chmod 777 /opt/madhura_kurhadkar_002769373_05
 sudo unzip "/opt/madhura_kurhadkar_002769373_05.zip" -d "/madhura_kurhadkar_002769373_05"
+
+echo ls -ld /madhura_kurhadkar_002769373_05
 
 cd /madhura_kurhadkar_002769373_05 || exit
 
