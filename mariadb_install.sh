bad42588e4ed5dc2c44041870c98d6e80f11efa5
Madhura Sanjay Kurhadkar <kurhadkar.m@northeastern.edu>
2023-10-18 17:22:41 -0400

    updated script

--- mariadb_install.sh
+++ mariadb_install.sh
@@ -37,12 +37,12 @@ sudo chmod 755 /madhura_kurhadkar_002769373_05
 
 sudo unzip madhura_kurhadkar_002769373_05 -d madhura_kurhadkar_002769373_05_app
 
-cd madhura_kurhadkar_002769373_05 || exit
+cd madhura_kurhadkar_002769373_05_app || exit
 
 # echo "Hello, starting with project installation at $DEST_DIR"
 
 # # Create a .env file in the destination directory
-# cat <<EOL > "madhura_kurhadkar_002769373_05/.env"
+# cat <<EOL > "madhura_kurhadkar_002769373_05_app/.env"
 # DB_NAME='$DB_NAME'
 # DB_USERNAME='$DB_USERNAME'
 # DB_PASSWORD='$DB_PASSWORD'
@@ -50,8 +50,8 @@ cd madhura_kurhadkar_002769373_05 || exit
 # PORT=$PORT
 # EOL
 
-# echo ".env file has been created at: madhura_kurhadkar_002769373_05/.env"
-# cat "madhura_kurhadkar_002769373_05/.env"
+# echo ".env file has been created at: madhura_kurhadkar_002769373_05_app/.env"
+# cat "madhura_kurhadkar_002769373_05_app/.env"
 
 
 # Install project dependencies (if package.json exists)
