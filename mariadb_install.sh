b316b0dbd3fa1c52ab25397abb92326a55fc22f3
Madhura Sanjay Kurhadkar <kurhadkar.m@northeastern.edu>
2023-10-18 04:33:55 -0400

    Madhura kurhadkar neu feature (#83)
    * Delete .github/workflows/deno.yml
    
    * Ci1 (#6)
    
    * adding the application details
    
    * Revert "reverting the commits done on main branch"
    
    This reverts commit 3a9c9761603d7a883c42b47bceaea6a5b531a4d7.
    
    * updated test cases
    
    * Ci2 (#7)
    
    * adding the application details
    
    * Revert "reverting the commits done on main branch"
    
    This reverts commit 3a9c9761603d7a883c42b47bceaea6a5b531a4d7.
    
    * updated test
    
    * updated folder
    
    * updated folder
    
    * updated folder yaml
    
    * update
    
    * added packer workflow
    
    * updated packer file
    
    * updated packer file
    
    * updated the path in yml files
    
    * updated the path in yml files
    
    * rename packer fmt
    
    * .pck updated
    
    * .pck updated
    
    * updated installation file
    
    * check db connection
    
    * updated the workflow
    
    * updated yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml
    
    * update yml

--- mariadb_install.sh
+++ mariadb_install.sh
@@ -42,7 +42,7 @@ if [ -f "package.json" ]; then
     echo "Project dependencies have been installed."
 fi
 
-# Run your Node.js file
-echo "Running your Node.js file..."
-node server.js
-sudo apt-get clean
+# # Run your Node.js file
+# echo "Running your Node.js file..."
+# node server.js
+# sudo apt-get clean
