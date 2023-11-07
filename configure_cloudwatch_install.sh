115bdbb6f8cfb12cf42f1724dedfa831930269d6
Madhura Sanjay Kurhadkar <kurhadkar.m@northeastern.edu>
2023-11-07 13:13:03 -0500

    Cloud watch metrics (#135)
    * merging code from feature3 to cloudWatchMetrics (#8)
    
    * updated readme
    
    * added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch metrics
    
    * corrected path of metrics
    
    * coorected path
    
    * corrected path
    
    * corrected path
    
    * corrected path
    
    * Update configure_cloudwatch_install.sh
    
    * Update configure_cloudwatch_install.sh
    
    * corrected path
    
    * updated the cloud watch configure file
    
    * updated the path in cwagent config file
    
    * updated package.json
    
    * updated cloudwatch configure

--- configure_cloudwatch_install.sh
+++ configure_cloudwatch_install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Download and install CloudWatch Agent
-wget https://amazoncloudwatch-agent.s3.amazonaws.com/debian/amd64/latest/amazon-cloudwatch-agent.deb
+#wget https://amazoncloudwatch-agent.s3.amazonaws.com/debian/amd64/latest/amazon-cloudwatch-agent.deb
+wget https://amazoncloudwatch-agent-us-east-1.s3.us-east-1.amazonaws.com/debian/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i -E amazon-cloudwatch-agent.deb
 
 
 # Create the directory for CloudWatch Agent config
-sudo mkdir -p /opt/aws/amazon-cloudwatch-agent/etc/
+#sudo mkdir -p /opt/aws/amazon-cloudwatch-agent/etc/
 
 # Configure the CloudWatch Agent
-# cat <<EOL | sudo tee /opt/csye6225/madhura_kurhadkar_002769373_06/amazon-cloudwatch-agent.json
-cat <<EOL | sudo tee /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+cat <<EOL | sudo tee /opt/csye6225/madhura_kurhadkar_002769373_06/amazon-cloudwatch-agent.json
+#cat <<EOL | sudo tee /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 
 {
   "agent": {
@@ -22,7 +23,7 @@ cat <<EOL | sudo tee /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agen
       "files": {
         "collect_list": [
           {
-            "file_path": "/var/log/csye6225.log",
+            "file_path": "var/log/csye6225.log",
             "log_group_name": "csye6225",
             "log_stream_name": "webapp"
           }
