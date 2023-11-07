0b5825b9f2a4ba3209efb947fd67b8749ef51d73
Madhura Sanjay Kurhadkar <kurhadkar.m@northeastern.edu>
2023-11-07 00:33:25 -0500

    Cloud watch metrics (#133)
    * merging code from feature3 to cloudWatchMetrics (#8)
    
    * updated readme
    
    * added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch metrics
    
    * corrected path of metrics
    
    * coorected path
    
    * corrected path
    
    * corrected path
    
    * corrected path
    
    * Update configure_cloudwatch_install.sh
    
    * Update configure_cloudwatch_install.sh
    
    * corrected path
    
    * updated the cloud watch configure file

--- configure_cloudwatch_install.sh
+++ configure_cloudwatch_install.sh
@@ -22,14 +22,14 @@ cat <<EOL | sudo tee /opt/csye6225/madhura_kurhadkar_002769373_06/amazon-cloudwa
       "files": {
         "collect_list": [
           {
-            "file_path": "/var/log/csye6225.log", 
+            "file_path": "/var/log/csye6225.log",
             "log_group_name": "csye6225",
             "log_stream_name": "webapp"
           }
         ]
-      },
+      }
+    },
       "log_stream_name": "cloudwatch_log_stream"
-    }
   },
   "metrics": {
     "metrics_collected": {
