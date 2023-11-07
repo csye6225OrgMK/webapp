62a665c755939e7d9dd836313912e8f15c8839a1
Madhura Sanjay Kurhadkar <kurhadkar.m@northeastern.edu>
2023-11-07 13:06:17 -0500

    updated cloudwatch configure

--- configure_cloudwatch_install.sh
+++ configure_cloudwatch_install.sh
@@ -6,12 +6,11 @@ sudo dpkg -i -E amazon-cloudwatch-agent.deb
 
 
 # Create the directory for CloudWatch Agent config
-sudo mkdir -p /opt/aws/amazon-cloudwatch-agent/etc/
+# sudo mkdir -p /opt/aws/amazon-cloudwatch-agent/etc/
 
 # Configure the CloudWatch Agent
-# cat <<EOL | sudo tee /opt/csye6225/madhura_kurhadkar_002769373_06/amazon-cloudwatch-agent.json
-cat <<EOL | sudo tee /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
-
+cat <<EOL | sudo tee /opt/csye6225/madhura_kurhadkar_002769373_06/amazon-cloudwatch-agent.json
+#cat <<EOL | sudo tee /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 {
   "agent": {
     "metrics_collection_interval": 10,
@@ -22,7 +21,7 @@ cat <<EOL | sudo tee /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agen
       "files": {
         "collect_list": [
           {
-            "file_path": "/var/log/csye6225.log",
+            "file_path": "var/log/csye6225.log",
             "log_group_name": "csye6225",
             "log_stream_name": "webapp"
           }
--- mariadb_install.sh
+++ mariadb_install.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# DB_NAME="csye6225_MK"
-# DB_USERNAME="root"
-# DB_PASSWORD=""
-# DB_HOST="127.0.0.1"
-# PORT=8080
-
 # Update the package list to get the latest package information
 sudo apt-get update
 
@@ -20,28 +13,6 @@ sudo apt-get install -y nodejs npm
 # Install unzip
 sudo apt-get install -y unzip
 
-# Start and enable MariaDB service
-# sudo systemctl start mariadb
-# sudo systemctl enable mariadb
-
-# Set the root password for MariaDB
-# sudo mysqladmin -u root password "$ROOT_PASSWORD"
-# sudo mysql
-# sudo mysql --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY 'root'; FLUSH PRIVILEGES; CREATE DATABASE csye6225_MK;"
-
-# sudo mkdir -p ~/madhura_kurhadkar_002769373_06
-# sudo chmod 755 ~/madhura_kurhadkar_002769373_06
-# sudo unzip madhura_kurhadkar_002769373_06 -d ~/madhura_kurhadkar_002769373_06
-# cd ~/madhura_kurhadkar_002769373_06 || exit
-# # Install project dependencies (if package.json exists)
-# if [ -f "package.json" ]; then
-#     echo "Installing project dependencies..."
-#     sudo npm install
-#     echo "Project dependencies have been installed."
-# fi
-# # Run your Node.js file
-# echo "Run your Node.js file..."
-# node server.js
 sudo apt-get clean
 sudo apt remove git -y
 sudo groupadd csye6225
