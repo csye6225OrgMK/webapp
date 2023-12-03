ced1042af42b8392d49bcb9618c83f1cf69f1985
Madhura Sanjay Kurhadkar <kurhadkar.m@northeastern.edu>
2023-12-03 11:43:15 -0500

    incorporated assignment10 (#158)
    * merging code from feature3 to cloudWatchMetrics (#8)
    
    * updated readme
    
    * added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch
    
    * Added cloud watch metrics
    
    * corrected path of metrics
    
    * coorected path
    
    * corrected path
    
    * corrected path
    
    * corrected path
    
    * Update configure_cloudwatch_install.sh
    
    * Update configure_cloudwatch_install.sh
    
    * corrected path
    
    * updated the cloud watch configure file
    
    * updated the path in cwagent config file
    
    * updated package.json
    
    * updated cloudwatch configure
    
    * updated paths in configure_cloudwatch_install.sh
    
    * added new logging
    
    * removed aws-sdk
    
    * removed aws-sdk
    
    * updated statsd
    
    * updated the delete assign,ent controller
    
    * updated the assignment controller
    
    * updated controller and logger
    
    * updated controller
    
    * updated webapp with Asignment submission
    
    * updated webapp
    
    * Updated assignment controller
    
    * Updated assignment controller
    
    * Updated assignment controller
    
    * Updated assignment controller, removed SNS publish on failure
    
    * Updated assignment controller, removed SNS publish on failure
    
    * updated the Assignment controller
    
    * updated the Assignment controller
    
    * updated the assignment controller
    
    * made changes for assignment 10

--- route/assignment.js
+++ route/assignment.js
@@ -6,25 +6,25 @@ const AssignmentController = require('../controller/Assignment');
 
 // Get all assignments or a particular created by authorized user
 router.get('/v1/assignments', AssignmentController.getAllAssignment);
-
-
-router.get('/v1/assignments/:id', AssignmentController.getAssignmentByID)
-    .all((req, res) => {
-        return res.status(405).end();
-    });
-
 // Create a new assignment (POST /api/assignments)
-router.post('/v1/assignments', AssignmentController.createAssignment)
-    .get(AssignmentController.getAllAssignment)
-    .all((req, res) => {
-        return res.status(405).end();
-    });
+router.post('/v1/assignments', AssignmentController.createAssignment);
+
+
+router.get('/v1/assignments/:id', AssignmentController.getAssignmentByID);
 
 //POST assignment submission
-router.post('/v1/assignments/:id/submission', AssignmentController.submitAssignment)
-    .all((req, res) => {
-        return res.status(405).end();
-    });
+router.post('/v1/assignments/:id/submission', AssignmentController.submitAssignment);
+router.all('/v1/assignments/:id/submission', (req, res) => {
+    // Return a 405 status code for any request other than POST
+    return res.status(405).send();
+});
+
+
+// Update an assignment (PUT /api/assignments/:id)
+router.put('/v1/assignments/:id', AssignmentController.updateAssignment);
+
+// Delete an assignment (DELETE /api/assignments/:id)
+router.delete('/v1/assignments/:id', AssignmentController.deleteAssignment);
 
 // Update an assignment (PATCH /api/assignments/:id)
 router.patch('*', AssignmentController.updateAssignmentPatch);
@@ -33,13 +33,6 @@ router.patch('*', AssignmentController.updateAssignmentPatch);
 router.head('*', AssignmentController.headRequest);
 
 router.options('*', AssignmentController.optionRequest);
-
-
-// Update an assignment (PUT /api/assignments/:id)
-router.put('/v1/assignments/:id', AssignmentController.updateAssignment);
-
-// Delete an assignment (DELETE /api/assignments/:id)
-router.delete('/v1/assignments/:id', AssignmentController.deleteAssignment);
 
 module.exports = router;
 
